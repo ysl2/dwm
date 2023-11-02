e808dbe5d2f2b9e0142c7b0b714803b71117b70c
ysl2 <www.songli.yu@gmail.com>
2023-11-02 12:47:16 +0800

    Extract to cyclelayouts.c

--- config.def.h
+++ config.def.h
@@ -61,6 +61,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
+#include "cyclelayouts.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
