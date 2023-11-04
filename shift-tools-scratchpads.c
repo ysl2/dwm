5ee39cec54f7e9225e0fb8e842f6035a20507b53
ysl2 <www.songli.yu@gmail.com>
2023-11-04 20:31:04 +0800

    Change shift-tools to flexipatch version.

--- shift-tools-scratchpads.c
+++ shift-tools-scratchpads.c
@@ -1,10 +1,12 @@
 static Arg shift(const Arg *arg, int clients);
 static void shiftboth(const Arg *arg);
+static void shiftbothclients(const Arg *arg);
 static void shiftswaptags(const Arg *arg);
-static void shifttag(const Arg *arg);
-static void shifttagclients(const Arg *arg);
+// static void shifttag(const Arg *arg);
+// static void shifttagclients(const Arg *arg);
 static void shiftview(const Arg *arg);
 static void shiftviewclients(const Arg *arg);
+static void swaptags(const Arg *arg);
 
 
 static Arg
@@ -16,35 +18,19 @@ shift(const Arg *arg, int clients)
 
 	shifted.ui = selmon->tagset[selmon->seltags];
 
-	#if TAGSYNC_PATCH
-	Monitor *origselmon = selmon;
-	for (selmon = mons; selmon; selmon = selmon->next)
-	#endif // TAGSYNC_PATCH
 	for (c = selmon->clients; c && clients; c = c->next) {
 		if (c == selmon->sel)
 			continue;
-		#if SCRATCHPADS_PATCH && !RENAMED_SCRATCHPADS_PATCH
 		if (!(c->tags & SPTAGMASK))
 			tagmask |= c->tags;
-		#elif SCRATCHPAD_ALT_1_PATCH
-		if (!(c->tags & SCRATCHPAD_MASK))
-			tagmask |= c->tags;
-		#else
-		tagmask |= c->tags;
-		#endif // SCRATCHPADS_PATCH
 	}
-	#if TAGSYNC_PATCH
-	selmon = origselmon;
-	#endif // TAGSYNC_PATCH
 
 	do {
 		if (arg->i > 0) // left circular shift
-			shifted.ui = (shifted.ui << arg->i) | (shifted.ui >> (NUMTAGS - arg->i));
+			shifted.ui = (shifted.ui << arg->i) | (shifted.ui >> (LENGTH(tags) - arg->i));
 		else // right circular shift
-			shifted.ui = (shifted.ui >> -arg->i) | (shifted.ui << (NUMTAGS + arg->i));
-		#if SCRATCHPADS_PATCH && !RENAMED_SCRATCHPADS_PATCH
+			shifted.ui = (shifted.ui >> -arg->i) | (shifted.ui << (LENGTH(tags) + arg->i));
 		shifted.ui &= ~SPTAGMASK;
-		#endif // SCRATCHPADS_PATCH
 	} while (tagmask && !(shifted.ui & tagmask));
 
 	return shifted;
@@ -58,6 +44,14 @@ shiftboth(const Arg *arg)
 	view(&shifted);
 }
 
+void
+shiftbothclients(const Arg *arg)
+{
+	Arg shifted = shift(arg, 1);
+	tag(&shifted);
+	view(&shifted);
+}
+
 /* swaps "tags" (all the clients) with the next/prev tag. */
 void
 shiftswaptags(const Arg *arg)
@@ -66,21 +60,21 @@ shiftswaptags(const Arg *arg)
 	swaptags(&shifted);
 }
 
-/* Sends a window to the next/prev tag */
-void
-shifttag(const Arg *arg)
-{
-	Arg shifted = shift(arg, 0);
-	tag(&shifted);
-}
-
-/* Sends a window to the next/prev tag that has a client, else it moves it to the next/prev one. */
-void
-shifttagclients(const Arg *arg)
-{
-	Arg shifted = shift(arg, 1);
-	tag(&shifted);
-}
+// /* Sends a window to the next/prev tag */
+// void
+// shifttag(const Arg *arg)
+// {
+// 	Arg shifted = shift(arg, 0);
+// 	tag(&shifted);
+// }
+//
+// /* Sends a window to the next/prev tag that has a client, else it moves it to the next/prev one. */
+// void
+// shifttagclients(const Arg *arg)
+// {
+// 	Arg shifted = shift(arg, 1);
+// 	tag(&shifted);
+// }
 
 void
 shiftview(const Arg *arg)
@@ -94,4 +88,25 @@ shiftviewclients(const Arg *arg)
 {
 	Arg shifted = shift(arg, 1);
 	view(&shifted);
+}
+
+void
+swaptags(const Arg *arg)
+{
+	Client *c;
+	unsigned int newtag = arg->ui & TAGMASK;
+	unsigned int curtag = selmon->tagset[selmon->seltags];
+
+	if (newtag == curtag || !curtag || (curtag & (curtag-1)))
+		return;
+
+	for (c = selmon->clients; c != NULL; c = c->next) {
+		if ((c->tags & newtag) || (c->tags & curtag))
+			c->tags ^= curtag ^ newtag;
+
+		if (!c->tags)
+            c->tags = newtag;
+	}
+
+	view(&((Arg) { .ui = newtag }));
 }
